baa6664c706453d5e07e4c61a551ac689772cd81
Mariana Vettorazzo <marianavettorazzo@gmail.com>
2021-04-14 01:20:15 -0300

    implantando detalhes visuais e estrutura de repetição

--- codigo.js
+++ codigo.js
@@ -1,15 +1,24 @@
 
-function questionario(escolha,vencedor,msgVencedor,msgPerdedor){
-    
+function questionario(escolha,vencedor,perdedor,msgVencedor,msgPerdedor,msgEscolhaErrada){
+   
     if(escolha == vencedor ){
-         return document.write(msgVencedor) 
-        
-    } else{
+        return document.write(msgVencedor) 
+    } else if(escolha == perdedor ) {
         return document.write(msgPerdedor)
+    } else {
+        return document.write(msgEscolhaErrada);
     }
+        
 }
 
 function agora(pergunta){ 
-    var ja = prompt(pergunta);
-      return parseInt(ja);
+      for( var i = 0;i<3;i++){
+       var ja = prompt(pergunta);
+          if(ja != "1" && ja != "2"){
+              alert("Valor inválido");
+              ja;
+          } else {
+                return(parseInt(ja));
+          }
+      }
 }
